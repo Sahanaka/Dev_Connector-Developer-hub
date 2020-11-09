b1113d7415b9b381b5b7362c19fef59e54e514a3
Sahanaka <weraniyagoda97@gmail.com>
2020-11-09 19:49:06 +0530

    Created the redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middelware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middelware))
+);
+
+export default store;
