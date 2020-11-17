39ea9e2981ea5afea512030bcea82f15b4109693
Sahanaka <weraniyagoda97@gmail.com>
2020-11-17 21:34:45 +0530

    Added Aboutus and profile top

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/Profile/Profile';
 import './App.css';
 
 // Redux
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -41,7 +41,7 @@ const EditProfile = ({ profile : { profile, loading }, createProfile, getCurrent
             youtube: loading || !profile.social ? '' : profile.social.youtube,
             instagram: loading || !profile.social ? '' : profile.social.instagram
           });
-    }, [loading]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
