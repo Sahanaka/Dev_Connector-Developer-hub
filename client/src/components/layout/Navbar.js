34920b9463ff8fa5c7fed79cb9e4404009e3fe48
Sahanaka <weraniyagoda97@gmail.com>
2020-11-17 13:31:59 +0530

    Display profiles added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,8 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout}) => {
     const authLinks = (
         <ul>
+            <li><Link to="/profiles">
+               Developers</Link></li>
             <li><Link to="/dashboard">
                 <i className="fas fa-user"></i>{' '}
                 <span className="hide-sm">Dashboard</span></Link></li>
@@ -21,7 +23,8 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout}) => {
 
     const guestLinks = (
         <ul>
-            <li><a href="#!">Developers</a></li>
+            <li><Link to="/profiles">
+               Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
         </ul>
