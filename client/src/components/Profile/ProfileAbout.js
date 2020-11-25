412fda44697df2d2de73c0051ef9f9af578fe4a5
Sahanaka <weraniyagoda97@gmail.com>
2020-11-25 22:15:34 +0530

    Refactored and cleanded app,js

--- client/src/components/Profile/ProfileAbout.js
+++ client/src/components/Profile/ProfileAbout.js
@@ -11,7 +11,7 @@ const ProfileAbout = ({
   <div className="profile-about bg-light p-2">
     {bio && (
       <Fragment>
-        <h2 className="text-primary">{name.trim().split(" ")[0]}s Bio</h2>
+        <h2 className="text-primary">{name.trim().split(" ")[0]}{" "}s Bio</h2>
         <p>{bio}</p>
         <div className="line" />
       </Fragment>
