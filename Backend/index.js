a77654b8c997c55c4030b0989b6103c5ad96a02e
Sahanaka <weraniyagoda97@gmail.com>
2020-11-26 15:33:41 +0530

    Fixed index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('../client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.resolve(____dirname, "client", "build", "index.html"));
     });
 }
 
